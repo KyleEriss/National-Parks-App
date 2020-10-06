707be786f3837e0b4e100ca6d10fefa81c1f9682
Kyle Eriss <kyle.eriss@gmail.com>
2020-10-06 00:20:10 -0400

    final revisions

--- app.js
+++ app.js
@@ -1,66 +1,60 @@
 const apiKey = "p1SOAI5F7toQx0FrMpVfCmD8rP6Ir7eUuMHaEDba";
 
-function getRepo() {
-    let userInput = document.getElementById("npsList").value;
+function getState(userInput, maxResults) {
     let dynamicUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${userInput}&api_key=${apiKey}`;
-    
-    //fetch URL
+
     fetch(dynamicUrl)
-    .then(handleErrors)
-    .then(response => console.log("ok") )
-    .catch(error => alert("Sorry, GitHub not found. Please check spelling."));
-        
-        // Resume program if file found
-    fetch(dynamicUrl)
-    .then(response => response.json())
-    .then(responseJson => displayResults(responseJson))
-    .catch(error => alert("Something went wrong. Try again later."));
+        .then(handleErrors)
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson, maxResults))
+        .catch(error => {
+            if (error.missingName) {
+                alert("Sorry, GitHub not found. Please check spelling.");
+            }
+            else {
+                alert("Something went wrong, try again later.");
+            }
+        });
 }
 
 
 
-    /*.then(response => {
-        if (response.ok) {
-            return respnse.json();
-        }
-        thro new Erro(response.statusText);
-    })
-    .then (responseJson => displayResults(responseJson))
-    .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });*/
-
-
 function handleErrors(response) {
     if (!response.ok) {
-        throw Error(response.statusText);
+        let error = Error(response.statusText);
+        error.missingName = true;
+        throw error;
     }
     return response;
 }
 
 
-function displayResults(responseJson) {
-    let userInput = document.getElementById("npsList").value;
-    console.log(userInput);
+function displayResults(responseJson, maxResults) {
     console.log(responseJson);
-    for (let i = 0; i < responseJson.data.length; i++) {
-        $('.results-list').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
-        <p>${responseJson.data[i].description}</p></li>`);
+    if (maxResults < responseJson.data.length) {
+        for (let i = 0; i < maxResults; i++) {
+            $('.results-list').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+            <p>${responseJson.data[i].description}</p></li>`);
+        }
+    }
+    else {
+        for (let i = 0; i < responseJson.data.length; i++) {
+            $('.results-list').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+            <p>${responseJson.data[i].description}</p></li>`);
+        }
     }
     $(".results").removeClass("hidden");
-  };
+};
 
 
 
 function watchForm() {
-    $("form").submit(event => {
+    $("form").on("click","#npsList", (event) => {
         event.preventDefault();
-        //let maxResults = $('#js-max-results').val();
-        if (document.getElementById("npsList").value === 0) {
-            return alert("Please enter GitHub handle");
-        }
         $(".results-list").empty();
-        getRepo();
+        let userInput = document.getElementById("npsList").value;
+        let maxResults = $('#js-max-results').val();
+        getState(userInput, maxResults);
     });
 }
 
